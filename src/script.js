79e2c367caaf4986049747a0f4704f8b258b801e
Peygy <64974600+Peygy@users.noreply.github.com>
2020-11-29 17:55:53 +0300

    Error

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function Dawn()
+{
+    // let div = document.createElement('div');
+    // let textNode = document.createTextNode('А вот и я');
+    // div.appendChild(textNode)
+    // div.style.width = "200";
+    // div.style.height = "200";
+    // div.style.backgroundColor = "black";
+    // document.body.appendChild(div);
+    alert("Ку");
+}
