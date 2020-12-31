ab5dc5470ed6838bf091ab0c7447348a57c5125f
Peygy <64974600+Peygy@users.noreply.github.com>
2020-12-31 18:08:42 +0300

    Fixes

--- src/script.js
+++ src/script.js
@@ -1,27 +1,5 @@
-var sumMain = 0;
-var minusMain = 0;
-var multiMain = 0;
-var divisionMain = 0;
-var outted = 0; 
-
 window.onload = function() 
 {
-    // let darkswitch = document.createElement('label');
-    // darkswitch.className = 'switch';
-    // document.body.appendChild(darkswitch);
-
-    // let darkDark = document.createElement('input');
-    // darkDark.type = 'checkbox';
-    // darkDark.id = 'dark';
-    // darkDark.addEventListener("change", DarkMode, false);
-    // darkswitch.appendChild(darkDark);
-
-    // let darkSpan = document.createElement('span');
-    // darkSpan.className = 'slider';
-    // darkswitch.appendChild(darkSpan);
-    
-
-
     let Calc = document.createElement('div');
     Calc.style.width = '600px';
     Calc.style.height = '600px';
@@ -32,20 +10,42 @@ window.onload = function()
     inputLine.style.display = 'inline-block';
     inputLine.style.height = '60px';
     inputLine.style.boxSizing = 'border-box';
+    inputLine.type = 'number';
     inputLine.id = 'Line';
-    inputLine.addEventListener("", Output, false);
+    inputLine.readOnly = true;
     Calc.appendChild(inputLine);
+
+    let BackSpace = document.createElement('input');
+    BackSpace.style.width = '10%';
+    BackSpace.style.display = 'inline-block';
+    BackSpace.type = 'button';
+    BackSpace.style.height = '61px';
+    BackSpace.id = 'Back';
+    BackSpace.value = '<---';
+    BackSpace.setAttribute("onclick", "Delete()");
+    Calc.appendChild(BackSpace);
+
+    let Cleaning = document.createElement('input');
+    Cleaning.style.width = '10%';
+    Cleaning.style.display = 'inline-block';
+    Cleaning.type = 'button';
+    Cleaning.style.height = '61px';
+    Cleaning.id = 'clean';
+    Cleaning.value = 'C';
+    Cleaning.setAttribute("onclick", "Clean()");
+    Calc.appendChild(Cleaning);
 
 
 
     let checking = document.createElement('label');
     checking.className = 'switch';
+    checking.setAttribute("onclick", "Checking()");
     Calc.appendChild(checking);
 
     let checkInput = document.createElement('input');
     checkInput.type = 'checkbox';
     checkInput.id = 'checkbox';
-    checkInput.addEventListener("change", Checking, false);
+    checkInput.setAttribute("onclick", "GetNumber(this)");
     checking.appendChild(checkInput);
 
     let checkSpan = document.createElement('span');
@@ -66,16 +66,14 @@ window.onload = function()
     one_num.id = '1';
     one_num.type = 'button';
     one_num.value = one_num.id;
-    one_num.classList.add("numbers");
-    one_num.addEventListener("click", GetNumber, false);
+    one_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(one_num);
 
     let two_num = document.createElement('input');
     two_num.id = '2';
     two_num.type = 'button';
     two_num.value = two_num.id;
-    two_num.classList.add("numbers");
-    two_num.addEventListener("click", GetNumber, false);
+    two_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(two_num);
     
     
@@ -83,15 +81,14 @@ window.onload = function()
     three_num.id = '3';
     three_num.type = 'button';
     three_num.value = three_num.id;
-    three_num.classList.add("numbers");
-    three_num.addEventListener("click", GetNumber, false);
+    three_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(three_num);
 
     let sum = document.createElement('input');
     sum.id = '+';
     sum.type = 'button';
     sum.value = sum.id;
-    sum.classList.add("actions");
+    sum.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(sum);
 
 
@@ -100,31 +97,28 @@ window.onload = function()
     four_num.id = '4';
     four_num.type = 'button';
     four_num.value = four_num.id;
-    four_num.classList.add("numbers");
-    four_num.addEventListener("click", GetNumber, false);
+    four_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(four_num);
 
     let five_num = document.createElement('input');
     five_num.id = '5';
     five_num.type = 'button';
     five_num.value = five_num.id;
-    five_num.classList.add("numbers");
-    five_num.addEventListener("click", GetNumber, false);
+    five_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(five_num);
     
     let six_num = document.createElement('input');
     six_num.id = '6';
     six_num.type = 'button';
     six_num.value = six_num.id;
-    six_num.classList.add("numbers");
-    six_num.addEventListener("click", GetNumber, false);
+    six_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(six_num);
 
     let minus = document.createElement('input');
     minus.id = '-';
     minus.type = 'button';
     minus.value = minus.id;
-    minus.classList.add("actions");
+    minus.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(minus);
 
 
@@ -133,31 +127,28 @@ window.onload = function()
     seven_num.id = '7';
     seven_num.type = 'button';
     seven_num.value = seven_num.id;
-    seven_num.classList.add("numbers");
-    seven_num.addEventListener("click", GetNumber, false);
+    seven_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(seven_num);
 
     let eight_num = document.createElement('input');
     eight_num.id = '8';
     eight_num.type = 'button';
     eight_num.value = eight_num.id;
-    eight_num.classList.add("numbers");
-    eight_num.addEventListener("click", GetNumber, false);
+    eight_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(eight_num);
  
     let nine_num = document.createElement('input');
     nine_num.id = '9';
     nine_num.type = 'button';
     nine_num.value = nine_num.id;
-    nine_num.classList.add("numbers");
-    nine_num.addEventListener("click", GetNumber, false);
+    nine_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(nine_num);
     
     let multi = document.createElement('input');
-    multi.id = 'X';
+    multi.id = '*';
     multi.type = 'button';
-    multi.value = multi.id;
-    multi.classList.add("actions");
+    multi.value = 'X';
+    multi.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(multi);
 
 
@@ -167,30 +158,27 @@ window.onload = function()
     dot.id = '.';
     dot.type = 'button';
     dot.value = dot.id;
-    dot.classList.add("actions");
     numsActions.appendChild(dot);
 
     let zero_num = document.createElement('input');
     zero_num.id = '0';
     zero_num.type = 'button';
     zero_num.value = zero_num.id;
-    zero_num.classList.add("numbers");
-    zero_num.addEventListener("click", GetNumber, false);
+    zero_num.setAttribute("onclick", "GetNumber(this)");
     numsActions.appendChild(zero_num);
-
-    let division = document.createElement('input');
-    division.id = '/';
-    division.type = 'button';
-    division.value = division.id;
-    division.classList.add("actions");
-    numsActions.appendChild(division);
 
     let equally = document.createElement('input');
     equally.id = '=';
     equally.type = 'button';
     equally.value = equally.id;
-    equally.classList.add("total");
     numsActions.appendChild(equally);
+
+    let division = document.createElement('input');
+    division.id = '/';
+    division.type = 'button';
+    division.value = '÷';
+    division.setAttribute("onclick", "GetNumber(this)");
+    numsActions.appendChild(division);
 }
 
 
@@ -202,12 +190,12 @@ function Checking()
 {
     if(document.getElementById('checkbox').checked)
     {
-        alert("Можно");
+        // alert("Можно");
         document.getElementById('Line').readOnly = false;
     }
     else
     {
-        alert("Нельзя");
+        // alert("Нельзя");
         document.getElementById('Line').readOnly = true;
     }
 }
@@ -216,52 +204,29 @@ function Checking()
 
 
 
-function GetNumber(event) // Забирает число, добавляет его в строку и в метод выполнения операций
+function GetNumber(num) // Забирает число, добавляет его в строку и в метод выполнения операций
 {
-    return Number (document.getElementsByClassName('numbers').innerHTML += event.value);    
+    document.getElementsById('Line').value += num.value; 
 }
-function GetActions(event) // Забирает оперцаю(действие) и добавляет его в строку... Исп. switch производим оперцаю и записываем результат в объект, который идёт в метод по ввыводу в строку
+function Delete()
 {
-    return document.getElementsByClassName('actions').innerHTML += event.value;
+    var line = document.getElementsById('Line').value;
+    document.getElementsById('Line').value = line.substring(0, line.length - 1);
 }
-function SetAction()
+function Equal()
 {
-    switch(GetActions)
-    {
-        case '+':
-            sumMain = sumMain + GetNumber;
-            break;
-        case '-':
-            minusMain = minusMain + GetNumber;
-            break;
-        case '*':
-            multiMain = multiMain + GetNumber;
-            break;
-        case '/':
-            divisonMain = divisionMain + GetNumber;
-            break;
-    }
-    return outted = sumMain + minusMain + multiMain + divisionMain;
+    var line = document.getElementsById('Line').value;
+    document.getElementsById('Line').value = eval(line);
 }
-function Output()
+function Clean()
 {
-    document.getElementById('Line').value = SetAction;
+    document.getElementsById('Line').value = " ";
 }
 
 
 
 
-// function DarkMode()
-// {
-//     if(document.getElementById('dark').checked)
-//     {
-//         document.body.style.backgroundColor = '#474747';
-//     }
-//     else
-//     {
-//         document.body.style.backgroundColor = 'whitesmoke'
-//     }
-// }
+
 
 
 
